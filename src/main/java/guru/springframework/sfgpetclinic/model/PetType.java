b94f22f1bf004c5872a62984c0c25efe5eba6b04
Ryan <ryan@Ryans-Mac-mini.fios-router.home>
2018-12-15 12:07:14 -0500

    Committing Model POJOs - closes #2.

---
+++ src/main/java/guru/springframework/sfgpetclinic/model/PetType.java
@@ -0,0 +1,17 @@
+package guru.springframework.sfgpetclinic.model;
+
+public class PetType {
+	
+	private String petName;
+
+	public String getPetName() {
+		return petName;
+	}
+
+	public void setPetName(String petName) {
+		this.petName = petName;
+	}
+	
+	
+
+}
